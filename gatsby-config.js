a5373e318c2779f835ee4cd977b9cce9703cb5ea
Sébastien Vercammen <contact@sebastienvercammen.be>
2020-02-12 18:37:25 +0100

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require(`dotenv`).config({
 module.exports = {
   siteMetadata: {
     siteTitle: `Sébastien Vercammen`,
-    siteTitleAlt: `Sébastien Vercammen`,
+    siteTitleAlt: `Sébastien Vercammen - Back-End Web Developer`,
   },
   plugins: [
     {
